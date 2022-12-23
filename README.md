4fc4008cf2031835591737b9750545104b7ba419
Gnatannn <96788997+Gnatannn@users.noreply.github.com>
2022-12-23 22:26:54 +0200

    2

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # goit-node-hw-01-CLI-app
+
+1. https://prnt.sc/cMoagVWfsiyx
+2. https://prnt.sc/wlGRztV6mmw_
+3. https://prnt.sc/DZDhoBUggvBe
+4. https://prnt.sc/TxrlfKgiFXJI
---
+++ index.js
@@ -0,0 +1,49 @@
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
+
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: рефакторить
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contactList = await listContacts();
+      console.log(contactList);
+      break;
+
+    case "get":
+      const selectedContact = await getContactById(id);
+      console.log(selectedContact);
+      break;
+
+    case "add":
+      const newContact = await addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removedContact = await removeContact(id);
+      console.log(removeContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
